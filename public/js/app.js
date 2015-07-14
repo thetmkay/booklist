0927f6bb1022a90faeee9519613e8ba4b0587bd0
George Nishimura <me@georgenishimura.com>
2015-07-14 14:42:22 +0100

    Add async font loading

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,23 @@
 	
 	$(document).ready(function() {
 		$('.bk-block').click(function() {
-			console.log('hi');
 			$(this).toggleClass('active');
 		});
+		
+		var fonts = ['Imperator','ImperatorBronzeSmallCaps','Sorts Mill Goudy','Rounded Elegance'];
+		var opts = [{},{},{'style':'italic'},{}];
+
+		for(var i = 0; i < fonts.length; i++) {
+			var font = fonts[i];
+			var observer = new FontFaceObserver(font, opts[i]);
+			observer.check().then((function(font) {
+				var fontclass = font.toLowerCase().replace(' ','-');
+				console.log(fontclass);
+				document.body.className += " " + fontclass;
+			}).bind(this,font), (function() {
+				console.error(font + " not available [font]");
+			}).bind(this,font));
+		}
 
 	});
 
