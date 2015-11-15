81656f28bb7a3db097c7c19fded4e6607ebee7d8
George Nishimura <thetmkay>
2015-11-15 01:36:30 +0000

    update to node 4, change from twitter to gdrive

--- app.js
+++ app.js
@@ -3,23 +3,17 @@ var express = require('express'),
   nunjucks = require('nunjucks'),
   _ = require('underscore'),
   path = require('path'),
+  config = require('./config'),
+  footer = require('gn-components/footer'),
+  GoogleSpreadsheet = require('google-spreadsheet'),
   moment = require('moment');
 
-var Twitter = require('twitter-node-client').Twitter,
-	config = {
-		"consumerKey":process.env.BLT_KEY,
-		"consumerSecret":process.env.BLT_SECRET,
-		"accessToken":process.env.BLT_TOKEN,
-		"accessTokenSecret":process.env.BLT_TOKEN_SECRET,
-		"callbackUrl":""
-	},
-	twitter = new Twitter(config);
-
+var bookSheet = new GoogleSpreadsheet(config.google.spreadsheet);
 
 var app = module.exports = express();
 
 var errorFn = function(err, response, body) {
-	console.error(err);
+	console.error(`ERROR: ${err}`);
 }
 
 var processTweetText = function(text) {
@@ -39,44 +33,24 @@ var processTweetText = function(text) {
 }
 
 var convertDate = function(date) {
-	var sd = date.split(/\s/);
-	var year = parseInt(sd[5]);
-	var day = parseInt(sd[2]);
-	var month = sd[1];
-	var time = sd[3];
-
-	var ds = sd[0] + ', ' + day + ' ' + month + ' ' + year + ' ' + time; 
-	return Date.parse(ds);
+	return moment(date,'MMMM DD, YYYY at h:mA').format('l');
 }
 
 var tweets = require('./booklist');
 
 function getBooks(callback) {
-
-
-	twitter.getSearch({'q':'from:thetmkay #Read #booklist'}, errorFn, function(data) {
-		var json = JSON.parse(data);
-		for(var i = 0; i < json.statuses.length; i++) {
-			var tweet = json.statuses[i];
-			var textblock = processTweetText(tweet.text);
-			if(!textblock) {
-				continue;
-			}
-			if(_.find(tweets, function(twt) { return tweet.id === twt.id }) === undefined) {
-				tweets.push({
-					id: tweet.id,
-					title: textblock.title,
-					author: textblock.author,
-					text: textblock.text,
-					date: tweet.created_at
-				});
-			}
+	bookSheet.getRows(1,{'start-index':2,'reverse':false},function(err,rows) {
+		if(err) {
+			callback(err,null);
 		}
-		var books = _.sortBy(tweets, function(twt) {
-			var timeSince = convertDate(twt.date);
-			return timeSince;
-		}).reverse();
-		callback(books);
+		var books = [];
+		//go in rev-chrono order
+		for(var i = rows.length - 1; i >= 0; i--) {
+			var book = processTweetText(rows[i].text);
+			book['date'] = convertDate(rows[i].date);
+			books.push(book);
+		}
+		callback(null,books);
 	});
 }
 
@@ -86,15 +60,10 @@ function getBooks(callback) {
 
 var view_paths = [
   path.join(__dirname,'views'),
-  path.join(__dirname, 'node_modules', 'gn_components', 'views'),
-  path.join(__dirname, '..', 'gn_components', 'views')
+  footer.html
 ];
 
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader(view_paths));
-
-env.addFilter('date', function(date, format) {
-  return moment(new Date(date)).format(format);
-});
 
 env.express(app);
 
@@ -113,7 +82,12 @@ app.set('view engine', 'html');
  */
 
 app.get('/', function(req,res) {
-  	getBooks(function(books) {
+  	getBooks(function(err,books) {
+
+		if(err) {
+			console.error(err);
+		}
+
 		res.render('index', {'books':books});
 	});
 });
